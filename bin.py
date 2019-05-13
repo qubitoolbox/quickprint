e22d968dca7f1eb33016bd474037ff99c66d2b66
qubitAI <osmanmorales@ymail.com>
2019-05-13 08:56:22 -0400

    Update bin.py

--- bin.py
+++ bin.py
@@ -1,5 +1,8 @@
 
+#Iterator that will print N values, given the iterator, 
+#it prints a binary value with its given decimal value.
 
-
-for i in range():
-  if not i % 10 == 0:  
+for i in range(699):
+  if not i % 10 == 0:
+    print(bin(i), ' = ' , '%d' % (i))
+    
