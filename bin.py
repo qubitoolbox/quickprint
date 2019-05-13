a32229a9f7259a99917437c723b66cb209aff1f1
qubitAI <osmanmorales@ymail.com>
2019-05-13 08:59:32 -0400

    Update bin.py

--- bin.py
+++ bin.py
@@ -1,6 +1,7 @@
 
 #Iterator that will print N values, given the iterator, 
-#it prints a binary value with its given decimal value.
+#it prints a binary value with its equivalent in decimal value. 
+#Values can be even, odd, divisible by N, etc.
 
 for i in range(699):
   if not i % 10 == 0:
